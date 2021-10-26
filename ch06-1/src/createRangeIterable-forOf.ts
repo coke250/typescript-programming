5f905d9d7c1a320c0ba1469b32680969b7b938ac
SSABI <fhy1930@gmail.com>
2021-10-27 00:36:47 +0900

    06-1 파일 수정

--- ch06-1/src/createRangeIterable-forOf.ts
+++ ch06-1/src/createRangeIterable-forOf.ts
@@ -1,4 +1,4 @@
 import { createRangeIterable } from "./createRangeIterable"
 const iterable = createRangeIterable(1, 3 + 1)
-for (let value of iterable)
+for (let value of iterable) // [Symbol.iterator]() 메서드가 없다는 오류 발생
   console.log(value)
