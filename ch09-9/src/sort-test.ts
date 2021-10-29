54b8fc8c04b5748ca642892736226db3babd4239
SSABI <fhy1930@gmail.com>
2021-10-30 00:35:16 +0900

    09-9 배열 다루기

---
+++ ch09-9/src/sort-test.ts
@@ -0,0 +1,9 @@
+import * as R from 'ramda'
+
+type voidToNumberFunc = () => number
+const makeRandomNumber = (max: number): voidToNumberFunc =>
+  (): number => Math.floor(Math.random() * max)
+
+const array = R.range(1, 5 + 1).map(makeRandomNumber(100))
+const sortedArray = R.sort((a: number, b: number): number => a - b)(array)
+console.log(array, sortedArray) // [ 94, 20, 11, 49, 32 ] [ 11, 20, 32, 49, 94 ]
---
+++ ch09-9/src/sortWith-test.ts
@@ -0,0 +1,10 @@
+import * as R from 'ramda'
+import { IPerson, makeRandomIPerson } from './models/person'
+import { displayPersons } from './displayPersons'
+
+const persons: IPerson[] = R.range(1, 4 + 1).map(makeRandomIPerson)
+const nameSortedPersons = R.sortWith([
+  R.descend(R.prop('name'))
+])(persons)
+
+displayPersons('sorted by name: ')(nameSortedPersons)
