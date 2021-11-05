2d9bded35e39cbe7f3fa138a1790a8b9c58e6fb8
ssabi <fhy1930@gmail.com>
2021-11-06 01:34:27 +0900

    10-6 nullable 타이봐 프로그램 안전성

---
+++ ch10-6/src/option/Option.ts
@@ -0,0 +1,8 @@
+import { Some } from './Some'
+import { None } from './None'
+
+export class Option {
+  private constructor() { }
+  static Some<T>(value: T) { return new Some<T>(value) }
+  static None = new None()
+}
