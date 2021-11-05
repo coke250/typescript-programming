c7ddfdcf4051d14f1a08d2eb6ccdf50ef75ff2c1
ssabi <fhy1930@gmail.com>
2021-11-06 00:43:49 +0900

    10-5 F-바운드 다형성

---
+++ ch10-5/src/test/Calculator-test.ts
@@ -0,0 +1,9 @@
+import { Calculator } from "../classes/Calculator"
+
+const value = (new Calculator(1))
+  .add(2)       // 3
+  .add(3)       // 6
+  .multiply(4)  // 24
+  .value()
+
+console.log(value)
